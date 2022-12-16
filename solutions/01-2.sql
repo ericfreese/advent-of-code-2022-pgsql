4917082a4ad946def4d6396581fe0c6b6ba2d404
Eric Freese <ericdfreese@gmail.com>
2022-12-16 08:27:36 -0700

    cleanup: Pull parsing into first step

--- solutions/01-2.sql
+++ solutions/01-2.sql
@@ -1,8 +1,8 @@
-with chunks as (
-  select
-    unnest(string_to_array(:'input', E'\n\n')) chunk
-),
-food_items as (
+with food_items as (
+  with chunks as (
+    select
+      unnest(string_to_array(:'input', E'\n\n')) chunk
+  )
   select
     row_number() over () elf_id,
     unnest(string_to_array(chunk, E'\n'))::integer calories
