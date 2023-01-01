eef37fe2b46b18e881264f9f49acf4c490ec24ad
Eric Freese <ericdfreese@gmail.com>
2023-01-01 16:00:44 -0700

    cleanup: Simplify a bit

--- solutions/09-1.sql
+++ solutions/09-1.sql
@@ -14,6 +14,7 @@ with motions as (
 head_positions as (
   select
     motion_number,
+    direction,
     array[
       coalesce(sum(case direction when 'R' then 1 when 'L' then -1 end) over (order by motion_number), 0),
       coalesce(sum(case direction when 'U' then 1 when 'D' then -1 end) over (order by motion_number), 0)
@@ -23,31 +24,26 @@ head_positions as (
 tail_positions as (
   with recursive tail_positions as (
     select * from (
-      values (0, array[0, 0]::bigint[])
+      values (0::bigint, array[0, 0]::bigint[])
     ) v(motion_number, position)
     union all
     select * from (
-      with delta as (
-        select
-          tp.motion_number + 1 motion_number,
-          hp.position head_position,
-          tp.position tail_position,
-          array[tp.position[1] - hp.position[1], tp.position[2] - hp.position[2]] delta
-        from tail_positions tp
-        inner join head_positions hp
-          on hp.motion_number = tp.motion_number + 1
-      )
       select
-        motion_number,
+        hp.motion_number,
         case
-          when (delta[1] between -1 and 1) and abs(delta[2]) > 1 then
-            array[head_position[1], head_position[2] + delta[2] / abs(delta[2])]
-          when abs(delta[1]) > 1 and (delta[2] between -1 and 1) then
-            array[head_position[1] + delta[1] / abs(delta[1]), head_position[2]]
+          when abs(hp.position[1] - tp.position[1]) > 1 or abs(hp.position[2] - tp.position[2]) > 1 then
+            case hp.direction
+              when 'U' then array[hp.position[1], hp.position[2] - 1]
+              when 'D' then array[hp.position[1], hp.position[2] + 1]
+              when 'R' then array[hp.position[1] - 1, hp.position[2]]
+              when 'L' then array[hp.position[1] + 1, hp.position[2]]
+            end
           else
-            tail_position
+            tp.position
         end
-      from delta
+      from tail_positions tp
+      inner join head_positions hp
+        on hp.motion_number = tp.motion_number + 1
     ) q
   )
   select *
